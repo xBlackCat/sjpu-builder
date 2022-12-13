66822a16b62a9aa8b887696ab111a6fc3e18d8b8
xBlackCat <xblackcat@gmail.com>
2022-12-13 22:44:36 +0300

    Migrate to Maven build system to proper support of Java module system

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+open module sjpu.builder {
+    exports org.xblackcat.sjpu.builder;
+    requires org.apache.commons.lang3;
+    requires org.javassist;
+    requires commons.logging;
+    requires org.apache.commons.text;
+}
