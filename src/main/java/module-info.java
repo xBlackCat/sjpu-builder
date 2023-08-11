33526ffe0d78ad80f70f0e3690fae4481c01bb55
xBlackCat <xblackcat@gmail.com>
2023-08-11 08:44:36 +0300

    Return to gradle build process. Do not use java modules for the library. Prepare for Gradle 9.0 release

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-open module sjpu.builder {
-    exports org.xblackcat.sjpu.builder;
-    requires org.apache.commons.lang3;
-    requires org.javassist;
-    requires commons.logging;
-    requires org.apache.commons.text;
-}
